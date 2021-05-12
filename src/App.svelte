15e1261135dcd8d8433cc907eb2863a4143fc76e
Tony Sullivan <tony.f.sullivan@gmail.com>
2021-05-12 17:32:47 +0200

    refactor: :recycle: Moving CSS assets from /src to /src/assets/css

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
   import "sanitize.css";
-  import "./global.css";
-  import Button from "./components/Button.svelte";
-
-  const onClick = () => console.log("clicked");
+  import "./assets/css/global.css";
+  import NavHeader from "./components/NavHeader.svelte";
 </script>
+
+<svelte:head>
+  <title>Home | Shop Kamfly</title>
+</svelte:head>
+
+<NavHeader />
 
 <main>
   <div class="container">
     <h1>Hello, World!</h1>
-    <Button label="Shop Now" on:click={onClick} />
   </div>
 </main>
