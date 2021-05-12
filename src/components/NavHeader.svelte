49fa0e31645a931f14d82b1f48e65c393bcc3483
Tony Sullivan <tony.f.sullivan@gmail.com>
2021-05-12 17:35:34 +0200

    feat: :sparkles: Adding in the basic nav header content
    Includes the logo (borrowed from kamfly.io) and a shopping cart icon button (not hooked up yet)

---
+++ src/components/NavHeader.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import logo from "../assets/icons/logo.svg";
+  import shoppingCart from "../assets/icons/shopping-cart.svg";
+
+  function openCart() {
+    console.log("open cart");
+  }
+</script>
+
+<header class="container">
+  <a href="/" alt="Our Shop">
+    {@html logo}
+  </a>
+
+  <button aria-label="Open Shopping Cart" on:click={openCart}>
+    {@html shoppingCart}
+  </button>
+</header>
+
+<style>
+  header {
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  a {
+    color: var(--color-blue-900);
+  }
+
+  a :global(svg) {
+    height: 1.5rem;
+    color: var(--color-brand);
+  }
+
+  button {
+    position: relative;
+    background: transparent;
+    border: none;
+    padding: 0.5em;
+    margin-right: -0.5em; /* visually align the icon's right edge */
+  }
+
+  button :global(svg) {
+    height: 2rem;
+  }
+</style>
