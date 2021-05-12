848f0f14c3cbbc6e824059532b678c01029541a7
Tony Sullivan <tony.f.sullivan@gmail.com>
2021-05-12 18:36:28 +0200

    test: :white_check_mark: Adding tests for getItemsCount
    Test coverage for empty cart, one item, multiple items, and invalid item state

---
+++ src/cartStore/__tests__/getItemsCount.test.ts
@@ -0,0 +1,74 @@
+import type { CartState } from '..'
+import { getItemsCount } from '../getItemsCount'
+
+test('should return 0 for an empty cart', () => {
+    const state: CartState = { items: [] }
+
+    const result = getItemsCount(state)
+    expect(result).toBe(0)
+})
+
+test('should return the quantity of a single cart item', () => {
+    const state: CartState = {
+        items: [
+            {
+                brand: 'Test Brand',
+                title: 'Oversize Blazer',
+                description: 'Black, size: 36',
+                quantity: 2,
+                itemPrice: 75
+            }
+        ]
+    }
+
+    const result = getItemsCount(state)
+    expect(result).toBe(2)
+})
+
+test('should return the total quantity of multiple cart items', () => {
+    const state: CartState = {
+        items: [
+            {
+                brand: 'Test Brand',
+                title: 'Oversize Blazer',
+                description: 'Black, size: 36',
+                quantity: 2,
+                itemPrice: 75
+            },
+            {
+                brand: 'Test Brand',
+                title: 'Undersize Blazer',
+                description: 'Black, size: 36',
+                quantity: 3,
+                itemPrice: 60
+            }
+        ]
+    }
+
+    const result = getItemsCount(state)
+    expect(result).toBe(5)
+})
+
+test('should ignore items with an invalid quantity', () => {
+    const state: CartState = {
+        items: [
+            {
+                brand: 'Test Brand',
+                title: 'Oversize Blazer',
+                description: 'Black, size: 36',
+                quantity: 2,
+                itemPrice: 75
+            },
+            {
+                brand: 'Test Brand',
+                title: 'Undersize Blazer',
+                description: 'Black, size: 36',
+                quantity: -3,
+                itemPrice: 60
+            }
+        ]
+    }
+
+    const result = getItemsCount(state)
+    expect(result).toBe(2)
+})
