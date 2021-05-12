be4aca4e5ee28d997a3d73eeabb3f52bdea47f08
Tony Sullivan <tony.f.sullivan@gmail.com>
2021-05-12 16:18:33 +0200

    chore: :fire: Removing template content, adding basic styling
    Removed all the template content, added sanitize.css as a vendor reset, and added a basic color pallet borrowed from coolors.co

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,14 @@
 <script lang="ts">
-	export let name: string;
+  import "sanitize.css";
+  import "./global.css";
+  import Button from "./components/Button.svelte";
+
+  const onClick = () => console.log("clicked");
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div class="container">
+    <h1>Hello, World!</h1>
+    <Button label="Shop Now" on:click={onClick} />
+  </div>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
